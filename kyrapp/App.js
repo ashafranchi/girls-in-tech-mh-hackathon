da02815cc151b36b067a33de87e377a73d64bbad
Asha <32620935+ashafranchi@users.noreply.github.com>
2021-11-16 21:21:49 -0800

    extra navigation working

--- kyrapp/App.js
+++ kyrapp/App.js
@@ -1,17 +1,45 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
+import NewScreen from './src/screens/NewScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+			    headerStyle: {backgroundColor: '#228CDB'},
+        	headerTintColor: '#fff'
+        }} initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={({navigation}) => ({
+            headerRight: () => (
+              <Icon 
+                name="plus" 
+                type="feather" 
+                color="#fff"
+                style={style.headerIcon}
+                onPress={() => navigation.navigate('New')}
+              />
+            )
+          })}
+        />
+        <Stack.Screen name="New" component={NewScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const style = StyleSheet.create({
+  headerIcon: {
+    marginRight: 10
+  }
+});
---
+++ kyrapp/src/screens/NewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function NewScreen () {
+  return (
+    <View>
+      <Text>This is New Screen</Text>
+    </View>
+  )
+}
