cc33c70e2b6e39b89cf9f042e94ec65c5b7777b9
Asha <32620935+ashafranchi@users.noreply.github.com>
2021-11-16 21:08:57 -0800

    initialized app

---
+++ kyrapp/App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
